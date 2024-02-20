3baecaab0f7bcf7d582f48a4e0d294bef44da479
Joaoferreiras <joaovictordesousaferreira1@gmail.com>
2024-02-20 06:59:54 -0300

    chore: criação da rota base

---
+++ src/routes/base.route.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+
+import packagejson from '../../package.json';
+
+export const baseRoutes = Router();
+
+baseRoutes.get('/', (_, res) => {
+  const { name, version, description, author } = packagejson;
+
+  res.status(200).json({ name, version, description, author });
+});
