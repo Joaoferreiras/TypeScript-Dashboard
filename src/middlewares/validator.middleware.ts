0a8ba59af4005b09c74a4173274553ac31464a49
Joaoferreiras <joaovictordesousaferreira1@gmail.com>
2024-03-03 17:14:41 -0300

    feat: formatando erro de validação

--- src/middlewares/validator.middleware.ts
+++ src/middlewares/validator.middleware.ts
@@ -1,5 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
 import { ZodRawShape, z } from 'zod';
+
+import { AppError } from '../errors/app.error';
 
 export enum ParamsType {
   QUERY = 'query',
@@ -16,8 +19,12 @@ export const validator = (params: ValidateParams) => {
     const result = z.object(params.schema).safeParse(req[params.type]);
 
     if (!result.success) {
-      console.log(result.error.issues);
+      const errorFomatted = result.error.issues.map(
+        (item) => `${item.path.join('.')}: ${item.message}`,
+      );
+      throw new AppError(errorFomatted, StatusCodes.UNPROCESSABLE_ENTITY);
     }
+    req[params.type] = result.data;
     next();
   };
 };
